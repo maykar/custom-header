08cc3be8c76ae21e3604b5d1fbdac9cf666a800d
maykar <25127328+maykar@users.noreply.github.com>
2019-11-04 14:13:04 +0000

    add hide_tabs

--- src/style-header.js
+++ src/style-header.js
@@ -1,3 +1,5 @@
+/* eslint radix: ["error", "as-needed"] */
+
 import { root } from "./get-root";
 import { config } from "./config";
 import { BuildHeader } from "./build-header";
@@ -9,37 +11,47 @@ export function styleHeader() {
   let style = document.createElement("style");
   style.setAttribute("id", "cch_header_style");
   style.innerHTML = `
-        cch-header {
-          width:100%;
-          display:flex;
-          justify-content: center;
-          background: ${config.background};
-          color: ${config.elements_color};
-          ${header.tabs.length === 0 ? "margin-top: 48px;" : ""}
-          ${config.footer ? "position: sticky; bottom: 0px;" : ""}
-        }
-        hui-view, hui-panel-view {
-          margin-top: -${headerHeight};
-          padding-top: ${config.footer ? "0px;" : "53px;"}
-          ${config.footer ? "padding-bottom: 48px;" : ""}
-          ${config.footer ? "margin-bottom: -48px;" : ""}
-        }
-        hui-panel-view {
-          ${config.footer ? "" : "padding-top: 48px;"}
-        }
-        #view {
-          ${config.footer ? "min-height: calc(100vh - 160px) !important;" : ""}
-        }
-        [buttonElem="menu"] {
-          ${config.menu_color ? `color: ${config.menu_color};` : ""}
-        }
-        [buttonElem="options"] {
-          ${config.options_color ? `color: ${config.options_color};` : ""}
-        }
-        [buttonElem="voice"] {
-          ${config.voice_color ? `color: ${config.voice_color};` : ""}
-        }
-      `;
+      cch-header {
+        width:100%;
+        display:flex;
+        justify-content: center;
+        background: ${config.background};
+        color: ${config.elements_color};
+        ${header.tabs.length === 0 ? "margin-top: 48px;" : ""}
+        ${config.footer ? "position: sticky; bottom: 0px;" : ""}
+      }
+      hui-view, hui-panel-view {
+        margin-top: -${headerHeight};
+        padding-top: ${config.footer ? "0px;" : "53px;"}
+        ${config.footer ? "padding-bottom: 48px;" : ""}
+        ${config.footer ? "margin-bottom: -48px;" : ""}
+      }
+      hui-panel-view {
+        ${config.footer ? "" : "padding-top: 48px;"}
+      }
+      #view {
+        ${config.footer ? "min-height: calc(100vh - 160px) !important;" : ""}
+      }
+      [buttonElem="menu"] {
+        ${config.menu_color ? `color: ${config.menu_color};` : ""}
+      }
+      [buttonElem="options"] {
+        ${config.options_color ? `color: ${config.options_color};` : ""}
+      }
+      [buttonElem="voice"] {
+        ${config.voice_color ? `color: ${config.voice_color};` : ""}
+      }
+    `;
+
+  Object.keys(config.tabs_color).forEach((tab) => {
+    style.innerHTML += `
+      paper-tabs > paper-tab:nth-child(${parseInt(tab) + 1}) {
+        color: ${config.tabs_color[tab]};
+        ${config.hide_tabs && config.hide_tabs[tab] ? "display: none;" : ""}
+      }
+    `;
+  });
+
   root.appendChild(style);
 
   // Hide cheverons completely when not visible.
