2f5c1e4775c87423d57f80d8435db25fcd9c584e
maykar <25127328+maykar@users.noreply.github.com>
2019-11-16 00:47:46 +0000

    delete unused file

--- src/custom-icons.js
+++
@@ -1,9 +0,0 @@
-import { root, main } from './helpers';
-import { header } from './build-header';
-
-const defaultTabIcons = {};
-export const customizeTabIcons = tabIconConfig => {
-  for (const tab in tabIconConfig) {
-    tab.querySelector('ha-icon').icon;
-  }
-};
