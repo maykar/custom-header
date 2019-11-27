103b0a04b23c32154cc3bfd230c308dec51b45fe
Ryan Meek <25127328+maykar@users.noreply.github.com>
2019-11-27 08:38:28 -0500

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -5,7 +5,7 @@ const sidebar = document.querySelector(".sidebar");
 resizeSidebarSpacer();
 window.addEventListener("resize", resizeSidebarSpacer());
 
-// Sidebar slide open animation at start.
+// Expand animation after 1s.
 setTimeout(() => {
   if (screen.width > 500) {
     header.style.marginLeft = "256px";
@@ -14,16 +14,19 @@ setTimeout(() => {
   }
 }, 1000);
 
-// Space in between top sidebar items and bottom needs calculated and run on resize.
+// Calculate and resize sidebar spacer.
 function resizeSidebarSpacer() {
   let space = 0;
   const sidebarContents = document.querySelector("paper-listbox").querySelectorAll("*")
   for (const element of sidebarContents) {
-    if (element.nodeName == "PAPER-ICON-ITEM") space += 52;
+    if (element.nodeName == "PAPER-ICON-ITEM") {
+      space += parseInt(window.getComputedStyle(element).getPropertyValue('height').replace('px','')) + 4;
+    }
   }
   document.querySelector(".spacer").style.height = `calc(100vh - ${space + 200}px)`;
 }
 
+// Expand/shrink sidebar.
 function expandSidebar() {
   if (header.style.marginLeft !== "256px") {
     header.style.marginLeft = "256px";
