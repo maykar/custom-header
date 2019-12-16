4027a5e0a2388cb955b7f6c6f991d21f02b4cc1a
ludeeus <joasoe@gmail.com>
2019-12-16 11:16:03 +0000

    Simplify rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,42 @@
-import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import nodeResolve from 'rollup-plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript2';
 import babel from 'rollup-plugin-babel';
-// import { terser } from 'rollup-plugin-terser';
+import serve from 'rollup-plugin-serve';
+import json from '@rollup/plugin-json';
+import { terser } from 'rollup-plugin-terser';
 
-export default {
-  input: ['src/custom-header.ts'],
-  output: {
-    dir: './dist',
-    format: 'es',
+const dev = process.env.ROLLUP_WATCH;
+
+const serveopts = {
+  contentBase: ['./dist'],
+  host: '0.0.0.0',
+  port: 5000,
+  allowCrossOrigin: true,
+  headers: {
+    'Access-Control-Allow-Origin': '*',
   },
-  plugins: [
-    resolve(),
-    typescript(),
-    babel({
-      exclude: 'node_modules/**',
-    }),
-    terser({ keep_fnames: true }),
-  ],
 };
+
+const plugins = [
+  nodeResolve({}),
+  commonjs(),
+  typescript(),
+  json(),
+  babel({
+    exclude: 'node_modules/**',
+  }),
+  dev && serve(serveopts),
+  !dev && terser({ keep_fnames: true }),
+];
+
+export default [
+  {
+    input: 'src/custom-header.ts',
+    output: {
+      dir: 'dist',
+      format: 'es',
+    },
+    plugins: [...plugins],
+  },
+];
