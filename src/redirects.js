7397624a047cac77e80afe23a410b7a9b5b9ffd1
maykar <25127328+maykar@users.noreply.github.com>
2019-12-28 22:10:05 +0000

    more default tab work

--- src/redirects.js
+++ src/redirects.js
@@ -49,13 +49,7 @@ export const redirects = (config, header) => {
   }
 
   // Click default tab on first open.
-  if (
-    !reloaded &&
-    defaultTab != null &&
-    !window.customHeaderDefaultClicked &&
-    haElem.tabs[defaultTab] &&
-    getComputedStyle(headerType.tabs[defaultTab]).display != 'none'
-  ) {
+  if (!reloaded && defaultTab != null && !window.customHeaderDefaultClicked && haElem.tabs[defaultTab]) {
     haElem.tabs[defaultTab].dispatchEvent(new MouseEvent('click', { bubbles: false, cancelable: false }));
   }
   window.customHeaderDefaultClicked = true;
